28ae1955adbf66de058d52a4501c44076c5166db
Jimmy Girardet <ijkl@netc.fr>
2026-02-04 21:51:52 +0100

    add nunito in header, remove font etension

--- header.php
+++ header.php
@@ -13,6 +13,9 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
 	<title>Collège Lyonnais des Généralistes Enseigants</title>
 
 	<?php wp_head(); ?>
